bc80aa90d52748a487a4a9aee598af6c6a04477e
jason <jason@zgwit.com>
2025-03-13 11:07:02 +0800

    电池优化

--- src/battery.lua
+++ src/battery.lua
@@ -12,6 +12,8 @@ local configs = require("configs")
 local default_config = {
     enable = true, -- 启用
     vbat = 3800, -- 供电电压mV（合宙的推荐设计是4.2）
+    adc = 0, -- 使用ADC0 adc.CH_VBAT
+    partial = true, -- 启用分压 adc.ADC_RANGE_1_2
     voltage = 12000, -- 电池电压
     empty = 11200, -- 空的电压
     full = 14200 -- 满的电压
@@ -34,11 +36,16 @@ function battery.init()
         return
     end
 
-    adc.open(adc.CH_VBAT)
-    local vbat = adc.get(adc.CH_VBAT)
-    adc.close(adc.CH_VBAT)
+    --分压
+    if config.adc ~= adc.CH_VBAT and config.partial then
+        adc.setRange(adc.ADC_RANGE_1_2)
+    end
 
-    log.info(tag, "init", vbat) --测试值 3835
+    adc.open(config.adc)
+    local vbat = adc.get(config.adc)
+    adc.close(config.adc)
+
+    log.info(tag, "init", vbat) -- 测试值 3835
 end
 
 --- 获取电池电量
@@ -50,19 +57,24 @@ function battery.get()
     end
 
     -- adc.setRange(config.range) -- 0-1.2v
-    adc.open(adc.CH_VBAT)
-    local vbat = adc.get(adc.CH_VBAT)
-    adc.close(adc.CH_VBAT)
-        
+    adc.open(config.adc)
+    local vbat = adc.get(config.adc)
+    adc.close(config.adc)
+
+    local target = 3800 -- 未电压
+    if  config.adc ~= adc.CH_VBAT and config.partial then
+        target = 1200
+    end
+
     -- 计算电压和百分比
-    local voltage = config.voltage * vbat / config.vbat
+    local voltage = config.voltage * vbat / target
     local percent = (voltage - config.empty) / (config.full - config.empty) * 100
     log.info(tag, "get", vbat, voltage, percent)
 
     return true, {
         vbat = vbat,
-        voltage = voltage,
-        percent = percent,
+        voltage = voltage > config.empty and voltage or 0,
+        percent = percent > 0 and percent or 0
     }
 end
 
