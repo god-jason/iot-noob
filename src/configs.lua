cb4da84c97b5e0b494b76f9b7330a0201fca8fcb
jason <jason@zgwit.com>
2025-03-14 10:32:19 +0800

    开启文件压缩

--- src/configs.lua
+++ src/configs.lua
@@ -10,17 +10,27 @@ local configs = {}
 
 ---加载配置文件，自动解析json
 ---@param name string 文件名，不带.json后缀
+---@param compress boolean 压缩
 ---@return boolean 成功与否
 ---@return table|nil
-function configs.load(name)
+function configs.load(name, compress)
     -- 找文件
     local path = "/" .. name .. ".json"
     -- if SD.enable then
     --     path = "/sd" .. path
     -- end
+    if compress then
+        if fastlz then
+            path = path .. ".flz"
+        elseif miniz then
+            path = path .. ".mz"
+        end
+    end
 
     -- 找不到，则下载
-    if not io.exists(path) then return false end
+    if not io.exists(path) then
+        return false
+    end
 
     -- 限制文件大小（780EPM已经到1MB了，不太需要）
     -- local size = io.fileSize(path)
@@ -30,6 +40,16 @@ function configs.load(name)
     -- end
 
     local data = io.readFile(path)
+
+    -- 解压
+    if compress then
+        if fastlz then
+            data = fastlz.uncompress(data)
+        elseif miniz then
+            data = miniz.uncompress(data)
+        end
+    end
+
     local obj, ret, err = json.decode(data)
     if ret == 1 then
         return true, obj
@@ -42,8 +62,9 @@ end
 ---保存配置文件，自动编码json
 ---@param name string 文件名，不带.json后缀
 ---@param data table|string 内容
+---@param compress boolean 压缩
 ---@return boolean 成功与否
-function configs.save(name, data)
+function configs.save(name, data, compress)
     if type(data) ~= "string" then
         data = json.encode(data)
     end
@@ -53,10 +74,26 @@ function configs.save(name, data)
     -- if SD.enable then
     --     path = "/sd" .. path
     -- end
+    if compress then
+        if fastlz then
+            path = path .. ".flz"
+        elseif miniz then
+            path = path .. ".mz"
+        end
+    end
 
     -- 删除历史(到底需不需要)，另外，是否需要备份
     if io.exists(path) then
         os.remove(path)
+    end
+
+    -- 压缩
+    if compress then
+        if fastlz then
+            data = fastlz.compress(data)
+        elseif miniz then
+            data = miniz.compress(data)
+        end
     end
 
     return io.writeFile(path, data)
