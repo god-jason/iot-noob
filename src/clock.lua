ccf664e9a943b044ca91910f813e3c8d73178621
jason <jason@zgwit.com>
2025-03-31 14:24:28 +0800

    时钟芯片

--- src/clock.lua
+++ src/clock.lua
@@ -12,20 +12,16 @@ local function bcd_to_hex(data)
 end
 
 local function hex_to_bcd(data)
-    return bit.lshift(math.floor(data / 10), 4) + data % 10
+    return bit.lshift(data // 10, 4) + data % 10
 end
-
 
 local default_options = {
     enable = false,
     chip = "SD3077", -- 型号
     i2c = 1, -- iic总线
     addr = 0x64, -- 站号 0x64: SD3077, 0x68 SD3231
-    init = {0x0E, 0x04}, -- 初始化 关闭clock输出
-    read = {0x00}, -- 读指令
-    write = {0x00}, -- 写指令
-    registers = { -- 寄存器
-    0x00, -- 首地址
+    reg = 0x00, -- 首地址
+    fields = { -- 寄存器
     "second", "minute", "hour", "wday", "day", "month", "year"}
 
 }
@@ -35,7 +31,7 @@ local options = {}
 --- rtc时钟芯片初始化
 function clock.init(opts)
     log.info(tag, "init")
-    
+
     -- 加载配置
     options = opts or default_options
 
@@ -43,31 +39,13 @@ function clock.init(opts)
         return
     end
 
-    log.info(tag, "init")
-
-    -- 兼容配置文件
-    if type(options.init) == "string" then
-        options.init = string.fromHex(options.init)
-    end
-    if type(options.read) == "string" then
-        options.read = string.fromHex(options.read)
-    end
-    if type(options.write) == "string" then
-        options.write = string.fromHex(options.write)
-    end
 
     -- 初始化iic接口
-    i2c.setup(options.i2c, i2c.FAST)
-
-    -- 初始化指令
-    if options.init ~= nil and #options.init > 0 then
-        i2c.send(options.i2c, options.addr, options.init)
-    end
+    i2c.setup(options.i2c, i2c.SLOW)
 
     -- 读取config芯片时钟
     sys.timerStart(clock.read, 500)
 end
-
 
 --- 读取芯片时钟
 --- @return boolean 成功与否
@@ -77,34 +55,32 @@ function clock.read()
         return false
     end
 
-    -- 发送
-    local ret = i2c.send(options.i2c, options.addr, options.registers[1]) -- 从秒开始读
-    if ret == false then
-        return ret
-    end
-
     -- 读取
-    local data = i2c.recv(options.i2c, options.addr, 7)
+    local data = i2c.readReg(options.i2c, options.addr, options.reg, 7)
+    if not data or #data ~= 7 then
+        return false
+    end
 
     -- 解析
     local time = {}
-    for i, v in ipairs(options.registers) do
+    for i, v in ipairs(options.fields) do
         if v == "year" then
-            time.year = bcd_to_hex(data:byte(i - 1)) + 2000
+            time.year = bcd_to_hex(data:byte(i )) + 2000
         elseif v == "month" then
-            time.mon = bcd_to_hex(bit.band(data:byte(i - 1), 0x7f)) - 1
+            time.mon = bcd_to_hex(bit.band(data:byte(i), 0x7f)) - 1
         elseif v == "day" then
-            time.day = bcd_to_hex(data:byte(i - 1))
+            time.day = bcd_to_hex(data:byte(i))
         elseif v == "wday" then
-            time.wday = bcd_to_hex(data:byte(i - 1)) + 1
+            time.wday = bcd_to_hex(data:byte(i)) + 1
         elseif v == "hour" then
-            time.hour = bcd_to_hex(bit.band(data:byte(i - 1), 0x7f)) -- 最高位代表 24小时制
+            time.hour = bcd_to_hex(bit.band(data:byte(i), 0x7f)) -- 最高位代表 24小时制
         elseif v == "minute" then
-            time.min = bcd_to_hex(data:byte(i - 1))
+            time.min = bcd_to_hex(data:byte(i))
         elseif v == "second" then
-            time.sec = bcd_to_hex(data:byte(i - 1))
+            time.sec = bcd_to_hex(data:byte(i))
         end
     end
+    log.info(tag, "read time", json.encode(time))
 
     -- 设置到系统中
     local r = rtc.set(time)
@@ -123,8 +99,8 @@ function clock.write()
     -- local tm = socket.ntptm()
     local tm = os.date("*t")
 
-    local data = { options.registers[1] }
-    for i, v in ipairs(options.registers) do
+    local data = {}
+    for i, v in ipairs(options.fields) do
         if v == "year" then
             table.insert(data, hex_to_bcd(tm.year - 2000))
         elseif v == "month" then
@@ -143,7 +119,7 @@ function clock.write()
     end
 
     -- 写指令
-    local ret = i2c.send(options.i2c, options.addr, data)
+    local ret = i2c.writeReg(options.i2c, options.addr, options.reg, data)
     log.info(tag, "write time", ret, json.encode(tm))
 
     return ret
