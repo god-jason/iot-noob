81b03807378fb8f8e5ed0003da8e6b7e125e3ed3
jason <jason@zgwit.com>
2025-09-19 07:41:57 +0800

    添加单位

--- protocols/protocol_cjt188.lua
+++ protocols/protocol_cjt188.lua
@@ -2,13 +2,62 @@
 -- @author 杰神
 -- @license GPLv3
 -- @copyright benyi 2025
-
 --- CJT188协议实现
 -- @module protocol_cjt188
-
 local Device = {}
 
 local tag = "cjt188"
+
+local function decodeBCD(len, str)
+    local ret = 0
+    for i = 1, len do
+        local b = str:byte(i)
+        local h = (b >> 4) & 0x0F
+        local l = b & 0x0F
+        ret = ret * 100 + h * 10 + l
+    end
+    return ret
+end
+
+local function encodeBCD(num, len)
+    local str = ""
+    for i = len, 1, -1 do
+        local b = num % 100
+        local h = (b // 10) & 0x0F
+        local l = b % 10
+        str = string.char((h << 4) | l) .. str
+        num = num // 100
+    end
+    return str
+end
+
+local units = {
+    [1] = "J",
+    [2] = "Wh",
+    [3] = "Whx10",
+    [4] = "Whx100",
+    [5] = "kWh",
+    [6] = "kWhx10",
+    [7] = "kWhx100",
+    [8] = "MWh",
+    [9] = "MWhx10",
+    [0xA] = "MWhx100",
+    [0xB] = "kJ",
+    [0xC] = "kJx10",
+    [0xD] = "kJx100",
+    [0xE] = "MJ",
+    [0xF] = "MJx10",
+    [0x10] = "MJx100",
+    [0x11] = "GJ",
+    [0x12] = "GJx10",
+    [0x13] = "GJx100",
+    [0x29] = "L",
+    [0x2A] = "Lx10",
+    [0x2B] = "Lx100",
+    [0x2C] = "m3",
+    [0x2D] = "m3x10",
+    [0x2E] = "m3x100"
+}
 
 ---创建设备
 -- @param master Master 主站实例
@@ -24,7 +73,7 @@ end
 
 function Device:open()
     log.info(tag, "open")
-    self.master.read(0,0,0,0)
+    self.master.read(0, 0, 0, 0)
 end
 
 ---读取数据
@@ -33,7 +82,7 @@ end
 -- @return any
 function Device:get(key)
     log.info(tag, "get", key)
-    self.master.read(0,0,0,0)
+    self.master.read(0, 0, 0, 0)
 end
 
 ---写入数据
@@ -42,7 +91,7 @@ end
 -- @return boolean 成功与否
 function Device:set(key, value)
     log.info(tag, "set", key, value)
-    self.master.read(0,0,0,0)
+    self.master.read(0, 0, 0, 0)
 end
 
 ---读取所有数据
@@ -50,7 +99,7 @@ end
 -- @return table|nil 值
 function Device:poll()
     log.info(tag, "poll")
-    self.master.read(0,0,0,0)
+    self.master.read(0, 0, 0, 0)
 end
 
 local Master = {}
@@ -96,13 +145,12 @@ function Master:write(slave, code, addr, data)
     self.link.ask("abc", 7)
 end
 
-
 ---打开主站
 function Master:open()
     log.info(tag, "open")
     local dev = Device:new()
     log.info(tag, dev)
-    table.insert(self.devices, dev) --先随便写，不测试
+    table.insert(self.devices, dev) -- 先随便写，不测试
 
 end
 
@@ -111,7 +159,6 @@ function Master:close()
     self.opened = false
     self.devices = {}
 end
-
 
 --- 轮询
 function Master:_polling()
@@ -154,5 +201,4 @@ function Master:_polling()
         end
     end
 end
-
 
