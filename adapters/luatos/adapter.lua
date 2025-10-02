0945759805824679aa4d4492e1b916957a0e2b38
Jason <jason@zgwit.com>
2025-10-02 22:28:30 +0800

    先写一半文档

--- adapters/luatos/adapter.lua
+++ adapters/luatos/adapter.lua
@@ -5,39 +5,74 @@ local iot = {}
 _G.sys = require("sys") -- 其实已经内置了
 _G.iot = iot -- 注册到全局
 
--- 定时器
+--- 定时任务
+-- @param func function 回调
+-- @param timeout integer 超时
+-- @return interger 定时器ID
 function iot.setTimeout(func, timeout, ...)
     return sys.timerStart(func, timeout, ...)
 end
+
+--- 循环定时任务
+-- @param func function 回调
+-- @param timeout integer 超时
+-- @return interger 定时器ID
 function iot.setInterval(func, timeout, ...)
-    return sys.timerloopStart(func, timeout, ...)
-end
-function iot.clearTimeout(id)
-    return sys.timerStop(id)
-end
-function iot.clearInterval(id)
-    return sys.timerStop(id)
+    return sys.timerLoopStart(func, timeout, ...)
 end
 
--- 协程管理
+--- 清空定时任务
+-- @param id interger 定时器ID
+function iot.clearTimeout(id)
+    sys.timerStop(id)
+end
+
+--- 清空循环定时任务
+-- @param id interger 定时器ID
+function iot.clearInterval(id)
+    sys.timerStop(id)
+end
+
+--- 创建协程
+-- @param func function 回调
+-- @param arg1 any 参数
+-- @return interger 任务ID
 function iot.start(func, ...)
     -- TODO 这里返回是协程对象，不是线程ID
     return sys.taskInit(func, ...)
 end
+
+--- 关闭协程
+-- @param id interger 任务ID
 function iot.stop(id)
     return false
 end
+
+--- 协程休眠
+-- @param timeout integer 超时
 function iot.sleep(timeout)
     sys.wait(timeout)
 end
+
+--- 协程休眠
+-- @param topic string 消息
+-- @param timeout integer 超时
+-- @return boolean 成功与否（超时false）
+-- @return any 传参
 function iot.wait(topic, timeout)
     return sys.waitUntil(topic, timeout)
 end
 
--- 消息机制
+--- 消息订阅
+-- @param topic string 消息
+-- @param func function 回调
 function iot.on(topic, func)
     sys.subscribe(topic, func)
 end
+
+--- 消息订阅（单次）
+-- @param topic string 消息
+-- @param func function 回调
 function iot.once(topic, func)
     local fn
     fn = function()
@@ -46,37 +81,81 @@ function iot.once(topic, func)
     end
     sys.subscribe(topic, fn)
 end
+
+--- 取消订阅
+-- @param topic string 消息
+-- @param func function 回调
 function iot.off(topic, func)
     sys.unsubscribe(topic, func)
 end
+
+--- 发布消息
+-- @param topic string 消息
+-- @param arg1 any 参数
 function iot.emit(topic, ...)
     sys.publish(topic, ...)
 end
 
--- 文件系统
+--- 打开文件
+-- @param filename string 文件名
+-- @param mode string 模式 r w a b + ...
+-- @return boolean 成功与否
+-- @return file 文件对象，通过file:read file:write file:seek file:close操作
 function iot.open(filename, mode)
     local fd = io.open(filename, mode)
     return fd ~= nil, fd
 end
+
+--- 文件是否存在
+-- @param filename string 文件名
+-- @return boolean 成功与否
 function iot.exists(filename)
     return io.exists(filename)
 end
+
+--- 读取文件全部内容
+-- @param filename string 文件名
+-- @return boolean 成功与否
+-- @return string 内容
 function iot.readFile(filename)
     local data = io.readFile(filename)
     return data ~= nil, data
 end
+
+--- 写入文件内容（覆盖）
+-- @param filename string 文件名
+-- @param data string 数据
+-- @return boolean 成功与否
 function iot.writeFile(filename, data)
     return io.writeFile(filename, data)
 end
+
+--- 写入文件内容（追加）
+-- @param filename string 文件名
+-- @param data string 数据
+-- @return boolean 成功与否
 function iot.appendFile(filename, data)
     return io.writeFile(filename, data, "ab+")
 end
+
+--- 创建目录
+-- @param path string 文件名
+-- @return boolean 成功与否
 function iot.mkdir(path)
     return io.mkdir(path)
 end
+
+--- 创建目录
+-- @param path string 文件名
+-- @return boolean 成功与否
 function iot.rmdir(path)
     return io.rmdir(path)
 end
+
+--- 遍历目录
+-- @param path string 文件名
+-- @param cb function(filename) 回调
+-- @param offset integer 跳过
 function iot.walk(path, cb, offset)
     offset = offset or 0
 
@@ -99,60 +178,130 @@ function iot.walk(path, cb, offset)
     end
 end
 
----- 加密模块
+--- MD5加密
+-- @param data string 数据
+-- @return string 加密后的十六进制串
 function iot.md5(data)
     return crypt.md5(data)
 end
+--- HMAC MD5加密
+-- @param data string 数据
+-- @param key string 秘钥
+-- @return string 加密后的十六进制串
 function iot.hmac_md5(data, key)
     return crypt.hmac_md5(data, key)
 end
+--- SHA1加密
+-- @param data string 数据
+-- @return string 加密后的十六进制串
 function iot.sha1(data)
     return crypt.sha1(data)
 end
+--- HMAC SHA1加密
+-- @param data string 数据
+-- @param key string 秘钥
+-- @return string 加密后的十六进制串
 function iot.hmac_sha1(data, key)
     return crypt.hmac_sha1(data, key)
 end
+--- SHA256加密
+-- @param data string 数据
+-- @return string 加密后的十六进制串
 function iot.sha256(data)
     return crypt.sha256(data)
 end
+--- HMAC SHA256加密
+-- @param data string 数据
+-- @param key string 秘钥
+-- @return string 加密后的十六进制串
 function iot.hmac_sha256(data, key)
     return crypt.hmac_sha256(data, key)
 end
+--- SHA512加密
+-- @param data string 数据
+-- @return string 加密后的十六进制串
 function iot.sha512(data)
     return crypt.sha512(data)
 end
+--- HMAC SHA512加密
+-- @param data string 数据
+-- @param key string 秘钥
+-- @return string 加密后的十六进制串
 function iot.hmac_sha512(data, key)
     return crypt.hmac_sha512(data, key)
 end
+
+--- 加密
+-- @param type string 类型 AES-128-ECB，AES-192-ECB，AES-256-ECB，AES-128-CBC，
+-- AES-192-CBC，AES-256-CBC，AES-128-CTR，AES-192-CTR，AES-256-CTR，AES-128-GCM，
+-- AES-192-GCM，AES-256-GCM，AES-128-CCM，AES-192-CCM，AES-256-CCM，DES-ECB，
+-- DES-EDE-ECB，DES-EDE3-ECB，DES-CBC，DES-EDE-CBC，DES-EDE3-CBC
+-- @param pading string 对齐 PKCS7，ZERO，ONE_AND_ZEROS，ZEROS_AND_LEN，NONE
+-- @param str string 明文
+-- @param key string 秘钥
+-- @param iv string IV
+-- @return string 密文
 function iot.encrypt(type, padding, str, key, iv)
     return crypt.encrypt(type, padding, str, key, iv)
 end
+
+--- 解密
+-- @param type string 类型
+-- @param pading string 对齐
+-- @param str string 密文
+-- @param key string 秘钥
+-- @param iv string IV
+-- @return string 明文
 function iot.decrypt(type, padding, str, key, iv)
     return crypto.decrypt(type, padding, str, key, iv)
 end
+--- BASE64编码
+-- @param data string 数据
+-- @return string base64编码
 function iot.base64_encode(data)
     return crypto.base64_encode(data)
 end
+--- BASE64解码
+-- @param data string base64编码
+-- @return string 数据
 function iot.base64_decode(data)
     return crypto.base64_decode(data)
 end
+--- CRC8校验
+-- @param data string 数据
+-- @return integer 检验
 function iot.crc8(data)
     return crypto.crc8(data)
 end
+--- CRC16校验
+-- @param method string 类型 IBM MAXIM USB MODBUS CCITT CCITT-FALSE X25 XMODEM DNP USER-DEFINED
+-- @param data string 数据
+-- @return integer 检验
 function iot.crc16(method, data)
     return crypto.crc16(method, data)
 end
+--- CRC32校验
+-- @param data string 数据
+-- @return integer 检验
 function iot.crc32(data)
     return crypto.crc32(data)
 end
 
--- SOCKET
 local Socket = require("socket.lua")
-iot.socket = function(opts)
+
+--- 创建SOCKET
+-- @param opts
+-- @return Socket
+function iot.socket(opts)
     return Socket:new(opts)
 end
 
--- HTTP
+--- HTTP请求
+-- @param url string URL
+-- @param opts table 参数 {method, headers, body}
+-- @return integer status
+-- @return table headers
+-- @return string body
 function iot.request(url, opts)
     opts = opts or {}
     local method = opts.method or "GET"
@@ -160,6 +309,13 @@ function iot.request(url, opts)
     local body = opts.body
     return http.request(method, url, headers, body)
 end
+--- HTTP下载
+-- @param url string URL
+-- @param dst string 目标路径
+-- @param opts table 参数 {method, headers, body}
+-- @return integer status
+-- @return table headers
+-- @return string body
 function iot.download(url, dst, opts)
     opts = opts or {}
     local method = opts.method or "GET"
@@ -173,24 +329,37 @@ end
 
 -- MQTT
 local MqttClient = require("mqtt_client.lua")
-iot.mqtt = function(opts)
+
+--- 创建MQTT
+-- @param opts table
+-- @return MqttClient
+function iot.mqtt(opts)
     return MqttClient:new(opts)
 end
 
--- GPIO接口
+--- GPIO接口
+-- @module GPIO
 local GPIO = {}
 GPIO.__index = GPIO
 
+--- 关闭
 function GPIO:close()
     gpio.close(self.id)
 end
+--- 设置电平
+-- @return level integer
 function GPIO:set(level)
     gpio.set(self.id, level)
 end
+--- 获取电平
+-- @return integer level
 function GPIO:get()
     return gpio.get(self.id)
 end
 
+--- 创建GPIO对象
+-- @param opts table 参数 {pull, callback, debounce}
+-- @return GPIO
 function iot.gpio(id, opts)
     opts = opts or {}
     local pull = opts.pull and gpio.PULLUP or gpio.PULLDOWN
@@ -213,24 +382,40 @@ function iot.gpio(id, opts)
 end
 
 -- 串口操作
+-- @module UART
 local UART = {}
 UART.__index = UART
 
+--- 关闭
 function UART:close()
     uart.close(self.id)
 end
+--- 写入
+-- @param data string 数据
+-- @return boolean 成功与否
 function UART:write(data)
     local len = uart.write(self.id, data)
     return len > 0, len
 end
+--- 读取
+-- @return boolean 成功与否
+-- @return string 数据
 function UART:read()
     local data = uart.read(self.id)
     return data ~= nil and #data > 0, data
 end
+--- 等待
+-- @param timeout integer 超时
+-- @return boolean 成功与否
+-- @return interger 数据长度
 function UART:wait(timeout)
     return sys.waitUntil("uart_receive_" .. self.id)
 end
 
+--- 创建UART对象
+-- @param id integer
+-- @param opts table 参数 {band_rate, data_bits, stop_bits, parity, rs485_gpio, rs485_level, rs485_delay}
+-- @return UART
 function iot.uart(id, opts)
     opts = opts or {}
 
