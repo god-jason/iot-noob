de17a568b77e996fd421d7e5b48fe57b66e581c7
Jason <jason@zgwit.com>
2025-12-31 06:42:23 +0800

    优化setting，独立时间戳

--- core/settings.lua
+++ core/settings.lua
@@ -1,42 +1,34 @@
 --- 对配置文件的封装，增加了时间戳，方便同步
 -- @module database
 local settings = {}
+local tag = "setting"
+
+-- local timestamp_name = "settings"
 
 local configs = require "configs"
 
 -- 所有参数
-local names = configs.load_default("settings", {})
+--local names = configs.load_default("settings", {})
 
-settings.timestamps = {}
+settings.timestamps = configs.load_default("timestamps", {})
 
 -- 加载配置
 function settings.load(name)
-    local config = configs.load_default(name, {})
-    settings[name] = config.data or {}
-    settings.timestamps[name] = config.timestamp or 0
-end
-
--- 加载所有配置
-function settings.init()
-    -- 逐一加载配置
-    for i, name in ipairs(names) do
-        settings.load(name)
-    end
+    settings[name] = configs.load_default(name, {})
 end
 
 -- 更新配置
 function settings.update(name, cfg)
+    settings.timestamps[name] = os.time()
     settings[name] = cfg
-    settings.save(name)
+    configs.save(name, cfg)
 end
 
 -- 保存配置
 function settings.save(name)
-    settings.timestamps[name] = os.time()
-    configs.save(name, {
-        data = settings[name],
-        timestamp = os.time()
-    })
+    if  settings[name] ~= nil then
+        configs.save(name,  settings[name])
+    end
 end
 
 return settings
