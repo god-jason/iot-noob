2d41620371a56f2a98b1276b5b037c6d47fa6106
Jason <jason@zgwit.com>
2026-03-24 13:37:17 +0800

    LBS和GPS定位

---
+++ components/gnss.lua
@@ -0,0 +1,103 @@
+--- GNSS模组（兼容合宙的GPS模组）
+-- @module GNSS
+local GNSS = require("utils").class(require("component"))
+
+require("components").register("gnss", GNSS)
+
+local log = iot.logger("GNSS")
+
+local exgnss = require("exgnss")
+
+function GNSS:init()
+    self.valid = false
+
+    if self.auto then
+        self:open()
+    end
+end
+
+function GNSS:open()
+    local opts = {
+        gnssmode = 1, -- 1 GPS+BD 2 BD
+        agps_enable = self.agps ~= false, -- 默认开启AGPS，定位更快
+        debug = self.debug == true
+    }
+    exgnss.setup(opts)
+
+    -- 定位成功回调
+    local function cb(mode)
+        local rmc = exgnss.rmc(2)
+        log.info("rmc", iot.json_encode(rmc))
+        self.valid = rmc.valid
+        self.latitude = rmc.lat
+        self.longitude = rmc.lng
+        self.speed = rmc.speed
+        self.course = rmc.course
+    end
+
+    -- 定位时长
+    local alive = self.alive or 60
+
+    if self.mode == "TIMER" then
+        -- 60s自动关闭
+        exgnss.open(exgnss.TIMER, {
+            tag = "TIMER",
+            val = alive,
+            cb = cb
+        })
+    elseif self.mode == "TIMERORSUC" then
+        -- 60s自动关闭，定位成功也关闭
+        exgnss.open(exgnss.TIMERORSUC, {
+            tag = "TIMERORSUC",
+            val = alive,
+            cb = cb
+        })
+    else
+        -- 一直开启
+        exgnss.open(exgnss.DEFAULT, {
+            tag = "DEFAULT",
+            cb = cb
+        })
+    end
+end
+
+function GNSS:close()
+    exgnss.close(exgnss.TIMER, {
+        tag = "TIMER"
+    })
+    exgnss.close(exgnss.TIMERORSUC, {
+        tag = "TIMERORSUC"
+    })
+    exgnss.close(exgnss.DEFAULT, {
+        tag = "DEFAULT"
+    })
+end
+
+
+--- 设置数据
+function GNSS:set(key, value)
+    log.info(self.pin, "set", key, value)
+    if key == "locate" then
+        self:open()
+    else
+        return false, "GNSS组件不支持变量：" .. key
+    end
+    return true
+end
+
+--- 获取数据
+function GNSS:get(key)
+    if key == "latitude" then
+        return true, self.latitude
+    elseif key == "longitude" then
+        return true, self.longitude
+    elseif key == "speed" then
+        return true, self.speed
+    elseif key == "course" then
+        return true, self.course
+    elseif key == "location" then
+        return true, {self.latitude, self.longitude} -- 数组形式
+    else
+        return false, "GNSS组件不支持变量：" .. key
+    end
+end
---
+++ components/lbs.lua
@@ -0,0 +1,58 @@
+--- LBS定位（使用合宙的LBS服务器）
+-- @module LBS
+local LBS = require("utils").class(require("component"))
+
+require("components").register("lbs", LBS)
+
+local log = iot.logger("LBS")
+
+function LBS:init()
+    self.latitude, self.longitude = 0, 0
+    -- 定时定位
+end
+
+function LBS:locate()
+    -- 如果配置了项目ID和KEY，就使用付费的airlbs
+    if self.project_id then
+        local ret, data = require("airlbs").request({
+            project_id = self.project_id,
+            project_key = self.project_key or PRODUCT_KEY
+        })
+        if ret then
+            self.latitude, self.longitude = data.lat, data.lng
+            return true, self.latitude, self.longitude
+        end
+    end
+
+    -- 默认使用免费的
+    local lat, lng, tm = require("lbsLoc2").request()
+    if lat == nil then
+        return false, "LBS服务器调用失败"
+    end
+    self.latitude, self.longitude = lat, lng
+    return true, self.latitude, self.longitude
+end
+
+--- 设置
+function LBS:set(key, value)
+    log.info(self.pin, "set", key, value)
+    if key == "locate" then
+        return self:locate()
+    else
+        return false, "LBS组件不支持变量：" .. key
+    end
+    return true
+end
+
+function LBS:get(key)
+    if key == "latitude" then
+        return true, self.latitude
+    elseif key == "longitude" then
+        return true, self.longitude
+    elseif key == "location" then
+        self:locate() -- 主动定一次位
+        return true, {self.latitude, self.longitude} -- 数组形式
+    else
+        return false, "LBS组件不支持变量：" .. key
+    end
+end
