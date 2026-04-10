e0edc78cceccc78b83f73adb1777c3d725617d9d
Jason <jason@zgwit.com>
2026-04-10 18:26:11 +0800

    新旧格式不一样

--- tools/compiler/main.go
+++ tools/compiler/main.go
@@ -18,23 +18,26 @@ var magic = []byte{0x5A, 0xA5, 0x5A, 0xA5}
 
 func pack(files []string) error {
 	buf := bytes.Buffer{}
+	l4 := make([]byte, 4)
+	l2 := make([]byte, 2)
 
 	//Magic
 	TLD(buf, 0x01, magic)
-	//版本号 2
-	TLD(buf, 0x02, []byte{0x00, 0x02})
-	//包头长度，固定值
-	TLD(buf, 0x03, []byte{0x00, 0x00, 0x00, 0x12})
 
-	l := make([]byte, 2)
-	binary.BigEndian.PutUint16(l, uint16(len(files)))
+	//版本号 2
+	binary.LittleEndian.PutUint16(l2, 2)
+	TLD(buf, 0x02, l2)
+
+	//包头长度，固定值
+	binary.LittleEndian.PutUint32(l4, 0x18)
+	TLD(buf, 0x03, l4)
 
 	//文件数量
-	TLD(buf, 0x04, l) //TODO 按实际数量
-	//CRC校验（无用）
-	TLD(buf, 0xFE, []byte{0xFF, 0xFF})
+	binary.LittleEndian.PutUint16(l2, uint16(len(files)))
+	TLD(buf, 0x04, l2) //TODO 按实际数量
 
-	ln := make([]byte, 4)
+	//CRC校验 TODO 实际算了
+	TLD(buf, 0xFE, []byte{0xFF, 0xFF})
 
 	//依次打包文件
 	//头
@@ -51,10 +54,10 @@ func pack(files []string) error {
 		}
 
 		//长度
-		binary.LittleEndian.PutUint32(ln, uint32(len(bs)))
-		TLD(buf, 0x03, ln)
+		binary.LittleEndian.PutUint32(l4, uint32(len(bs)))
+		TLD(buf, 0x03, l4)
 
-		//CRC检验（无用）
+		//CRC检验
 		TLD(buf, 0xFE, []byte{0xFF, 0xFF})
 
 		//文件内容
